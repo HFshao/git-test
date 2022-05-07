f85a401b532f5fb216d19940457f8b6f6bb0943b
shf <shf@cool.com>
2022-05-07 15:25:03 +0800

    master second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
